e3d3fd0594dabc3ec811c86518aa7990c35102c0
shirait <hamburgkarry@gmail.com>
2026-05-13 14:09:00 +0900

    認証のルーティングはv1配下にしないよう修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,18 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "profile", to: "profile#show"
-      devise_for(
-        :users,
-        path: "auth",
-        path_names: {
-          sign_in: "sign_in",
-          sign_out: "sign_out"
-        }
-      )
     end
   end
+
+  devise_for(
+    :users,
+    path: "auth",
+    path_names: {
+      sign_in: "sign_in",
+      sign_out: "sign_out"
+    }
+  )
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
