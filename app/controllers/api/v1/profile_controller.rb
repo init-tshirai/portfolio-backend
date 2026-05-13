28b3ca2b57f742e37e27d52a2a2dc0a89b9c7340
shirait <hamburgkarry@gmail.com>
2026-05-13 10:54:58 +0900

    apiエンドポイントのサンプルを追加

--- app/controllers/api/v1/profile_controller.rb
+++ app/controllers/api/v1/profile_controller.rb
@@ -1,4 +1,11 @@
 class Api::V1::ProfileController < ApplicationController
   def show
+    render(
+      json: {
+        name: "Teruaki Shirai",
+        role: "Web Developer",
+        message: "Rails APIから取得したプロフィールです。",
+      }
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get "profile/show"
+      get "profile", to: "profile#show"
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
