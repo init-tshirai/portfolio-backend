43c9ca1d449d6b33a13eed552c7fbbe4893f16ca
shirait <hamburgkarry@gmail.com>
2026-05-13 13:31:50 +0900

    rubocop -a

--- app/controllers/api/v1/profile_controller.rb
+++ app/controllers/api/v1/profile_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::ProfileController < ApplicationController
       json: {
         name: "Teruaki Shirai",
         role: "Web Developer",
-        message: "Rails APIから取得したプロフィールです。",
+        message: "Rails APIから取得したプロフィールです。"
       }
     )
   end
