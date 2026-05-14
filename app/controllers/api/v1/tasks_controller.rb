a3b8b0cfc9e6f1f13441e42b02f22d51785f50b7
shirait <hamburgkarry@gmail.com>
2026-05-14 13:45:37 +0900

    タスクの検索機能を追加

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -2,9 +2,8 @@ class Api::V1::TasksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    # todo: 検索機能実装 （users.nameで検索できるよう、eager_loadに変更する。）
     # todo: ページネーション実装
-    tasks = Task.preload(:user).order(due_date: :desc)
+    tasks = search_tasks(Task.preload(:user).order(due_date: :desc))
 
     render(
       json: tasks.map { |task|
@@ -12,12 +11,44 @@ class Api::V1::TasksController < ApplicationController
           id: task.id,
           title: task.title,
           status: task.status,
-          due_date: task.due_date.strftime("%Y-%m-%d"),
+          due_date: task.due_date&.strftime("%Y-%m-%d"),
           user: {
+            id: task.user.id,
             name: task.user.name
           }
         }
       }
     )
+  end
+
+  private
+
+  def search_tasks(tasks)
+    tasks = tasks.where("title LIKE ?", "%#{Task.sanitize_sql_like(params[:title])}%") if params[:title].present?
+    tasks = tasks.where(status: status_param) if params[:status].present?
+    tasks = tasks.where("due_date >= ?", due_date_from.beginning_of_day) if params[:due_date_from].present?
+    tasks = tasks.where("due_date <= ?", due_date_to.end_of_day) if params[:due_date_to].present?
+    tasks = tasks.where(user_id: params[:user_id]) if params[:user_id].present?
+
+    tasks
+  end
+
+  def status_param
+    status = params[:status]
+    return status if Task.statuses.key?(status)
+
+    raise ActionController::BadRequest, "Invalid status: #{status}"
+  end
+
+  def due_date_from
+    Date.iso8601(params[:due_date_from])
+  rescue Date::Error
+    raise ActionController::BadRequest, "Invalid due_date_from: #{params[:due_date_from]}"
+  end
+
+  def due_date_to
+    Date.iso8601(params[:due_date_to])
+  rescue Date::Error
+    raise ActionController::BadRequest, "Invalid due_date_to: #{params[:due_date_to]}"
   end
 end
